5d2d67adcd75a7fe36ac0d65b003ff8c5a28c59a
vGuerraF <viniciusguerraf@gmail.com>
2023-01-11 18:32:43 -0300

    feat: implement others resources

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -1,25 +1,35 @@
 const baseUrl = "http://localhost:3001/paletas";
 
 export const api = {
-  readAllPaletas: async () => {
-    const response = await fetch(baseUrl + "/find-paletas");
-    const allPaletas = await response.json();
-    return allPaletas;
-  },
-  readByIdPaletas: async (paleta) => {
-    fetch(baseUrl + "/find-paleta/:id", {
-      method: GET,
-      headers: new Headers({ "Content-Type": "application/json" }),
-      body: JSON.stringify(paleta),
-    });
-  },
-  createPaletas: async (paleta) => {
+  createPaleta: async (paleta) => {
     const response = await fetch(baseUrl + "/create-paletas", {
-      method: POST,
+      method: "POST",
       headers: new Headers({ "Content-Type": "application/json" }),
       body: JSON.stringify(paleta),
     });
     const newPaleta = await response.json();
     return newPaleta;
+  },
+
+  getAllPaletas: async () => {
+    const response = await fetch(baseUrl + "/find-paletas");
+    const allPaletas = await response.json();
+
+    return allPaletas;
+  },
+
+  getPaletaById: async (paletaId) => {
+    const response = await fetch(baseUrl + "/find-paleta/:" + paletaId);
+    const paletaFinded = await response.json();
+    return paletaFinded;
+  },
+
+  deletePaleta: async (paletaId) => {
+    const response = await fetch(baseUrl + "/delete-paleta/:" + paletaId, {
+      method: "DELETE",
+      headers: new Headers({ "Content-Type": "application/json" }),
+    });
+    const paletaDeleted = await response.json();
+    return paletaDeleted;
   },
 };
