66dd2015c789de75956a6b08e600116a7eef5ea8
vGuerraF <viniciusguerraf@gmail.com>
2023-01-11 18:16:29 -0300

    feat: implement form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,64 +1,74 @@
 import { useState } from "react";
-import { api } from "../../utils/api/api.js"
+import { api } from "../../utils/api/api";
 
-export function Form() {
-  const [NewPaleta, setNewPaleta] = useState();
+export function Form({ getAll, handleModal }) {
+  const [newPaleta, setNewPaleta] = useState({ characters: [] });
+  const [loading, setLoading] = useState(false);
 
-  api.createPaletas(NewPaleta)
-
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
+    setLoading(true);
     event.preventDefault();
 
-    setNewPaleta({ ...NewPaleta, character: [] });
+    await api.createPaleta(newPaleta);
+    await getAll();
+    setLoading(false);
+    handleModal();
   }
 
   return (
-    <div className="form">
-      <form onSubmit={handleSubmit} className="form-inputs">
-        <section>
-          <span>sabor:</span>
-          <input
-            type="text"
-            name="sabor"
-            onChange={(event) => {
-              setNewPaleta({ ...NewPaleta, sabor: event.target.value });
-            }}
-          ></input>
-        </section>
-        <section>
-          <span>descricao:</span>
-          <input
-            type="text"
-            name="descricao"
-            onChange={(event) => {
-              setNewPaleta({ ...NewPaleta, descricao: event.target.value });
-            }}
-          ></input>
-        </section>
-        <section>
-          <span>foto:</span>
-          <input
-            type="text"
-            name="foto"
-            onChange={(event) => {
-              setNewPaleta({ ...NewPaleta, foto: event.target.value });
-            }}
-          ></input>
-        </section>
-        <section>
-          <span>preco:</span>
-          <input
-            type="number"
-            name="preco"
-            onChange={(event) => {
-              setNewPaleta({ ...NewPaleta, preco: event.target.value });
-            }}
-          ></input>
-        </section>
-        <button type="submit" className="btn-submit">
-          Submit
-        </button>
-      </form>
-    </div>
+    <>
+      {loading ? (
+        <div> loading...</div>
+      ) : (
+        <div className="form">
+          <form onSubmit={handleSubmit} className="form-inputs">
+            <section>
+              <span>Sabor:</span>
+              <input
+                type="text"
+                name="sabor"
+                onChange={(event) => {
+                  setNewPaleta({ ...newPaleta, sabor: event.target.value });
+                }}
+              ></input>
+            </section>
+            <section>
+              <span>Descricao</span>
+              <input
+                type="text"
+                name="descricao"
+                onChange={(event) => {
+                  setNewPaleta({ ...newPaleta, descricao: event.target.value });
+                }}
+              ></input>
+            </section>
+            <section>
+              <span>Foto:</span>
+              <input
+                type="text"
+                name="foto"
+                onChange={(event) => {
+                  setNewPaleta({ ...newPaleta, foto: event.target.value });
+                }}
+              ></input>
+            </section>
+            <section>
+              <span>Preco:</span>
+              <input
+                type="number"
+                name="preco"
+                onChange={(event) => {
+                  setNewPaleta({ ...newPaleta, preco: event.target.value });
+                }}
+              ></input>
+            </section>
+            <button type="submit" className="btn-submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      )}
+    </>
   );
 }
+
